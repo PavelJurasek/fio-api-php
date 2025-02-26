3a71955f9980e7d2e9d2eb04420055f4e8aba7ab
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2025-02-26 16:00:20 +0100

    Downloader: Explicitly state nullability

--- src/FioApi/Downloader.php
+++ src/FioApi/Downloader.php
@@ -26,7 +26,7 @@ class Downloader
 
     public function __construct(
         string $token,
-        \GuzzleHttp\ClientInterface $client = null,
+        ?\GuzzleHttp\ClientInterface $client = null,
         ?string $savePath = null
     ) {
         $this->urlBuilder = new UrlBuilder($token);
